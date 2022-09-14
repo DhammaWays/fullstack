c037d3943c3a8eee151641807b94f69af7322582
Lekhraj <lekhraj@gmail.com>
2022-09-14 19:52:28 +0530

    Updated Confusion React App to show menu uvia custom Menu component: cards

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -8,10 +8,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+       
 
 /*
 ReactDOM.render(<App />, document.getElementById('root'));
