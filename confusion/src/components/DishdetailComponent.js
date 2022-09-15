51bbb6c81985f7a2daceb2225904d964d56cb325
Lekhraj <lekhraj@gmail.com>
2022-09-16 00:28:55 +0530

    Refactored react app further to create/use functional components

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,69 +1,72 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 
-/* DishDetail: Pure (no state) Presentational component to render details of given dish */
-class DishDetail extends Component {
- 
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-     }
+/* DishDetail: Pure (no state) Presentational component to render details of given dish
+ * Made it a functional component as we do not need overahead/functionality of class object.
+ */
 
-    renderComments(comments) {
-        if (comments != null) {
-            var commentsList = comments.map((comment) => {
-                /* We use Date class to convert our datetime string to US style date: Oct 15, 2014 */
-                var date = new Date(comment.date).toDateString().split(' ');
-                return (
-                    <ul key={comment.id} className="list-unstyled">
-                        <li>{comment.comment}</li>
-                        <li>--{comment.author}, {date[1]} {date[2]}, {date[3]}</li>
-                    </ul >
-                );
-            });
-
-            return (
-                <div>
-                    <h4>Comments</h4>
-                    {commentsList}
-                </div>
-            );
-        }
-        else { /* no comments; just return empty div */
-            return (
-                <div></div>
-                );
-        }
+function RenderDish({ dish }) {
+    if (dish != null) {
+        return (
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
     }
+}
 
-    render() { 
-        /* Use the "dish" property passed in if available */
-        if (this.props.dish != null) {
+function RenderComments({ comments }) {
+    if (comments != null) {
+        const commentsList = comments.map((comment) => {
+            /* We use Date class to convert our datetime string to US style date: Oct 15, 2014 */
+            var date = new Date(comment.date).toDateString().split(' ');
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
-                    </div>
+                <ul key={comment.id} className="list-unstyled">
+                    <li>{comment.comment}</li>
+                    <li>--{comment.author}, {date[1]} {date[2]}, {date[3]}</li>
+                </ul >
+            );
+        });
+
+        return (
+            <div>
+                <h4>Comments</h4>
+                {commentsList}
+            </div>
+        );
+    }
+    else { /* no comments; just return empty div */
+        return (
+            <div></div>
+        );
+    }
+}
+
+/* Functional Component:
+ * const DishDetail = props => {} is another way of writing: function DishDetail(props) {}
+ */
+const DishDetail = (props) => {
+    /* Use the "dish" property passed in if available */
+    if (props.dish != null) {
+        return (
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
                 </div>
-            );
-        }
-        else { /* no dish to render; return an empty div */
-            return (
-                <div></div>
-            );
-        }
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.dish.comments} />
+                </div>
+            </div>
+        );
+    }
+    else { /* no dish to render; return an empty div */
+        return (
+            <div></div>
+        );
     }
 }
 
