4db611be5a21862d0c887b63e54b868080963a25
Lekhraj <lekhraj@gmail.com>
2022-09-16 02:12:55 +0530

    Confusion React App: Added header and footer

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,8 +1,8 @@
 import { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
-
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 /* Main: Container component to manage overall presentation by using other presentation component
@@ -28,14 +28,11 @@ class Main extends Component {
     render() {
         return (
             <div className="App">
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header />
                 <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-             </div>
+                <Footer />
+            </div>
         );
     }
 }
