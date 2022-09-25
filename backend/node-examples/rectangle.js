bc65a2b5c34cf95c406468f295d230e1343a8ec3
Lekhraj <lekhraj@gmail.com>
2022-09-25 20:01:29 +0530

    Updated simple rectangle node to use callbacks for sync handling of errors and computation

--- backend/node-examples/rectangle.js
+++ backend/node-examples/rectangle.js
@@ -1,2 +1,15 @@
-exports.perimeter = (l, b) => (2 * (l + b));
-exports.area = (l, b) => (l * b);
+module.exports = (l,b,callback) => {
+    if (l <= 0 || b <= 0)
+        setTimeout(() => 
+            callback(new Error("Rectangle dimensions should be greater than zero: l = "
+                + l + ", and b = " + b), 
+            null),
+            2000);
+    else
+        setTimeout(() => 
+            callback(null, {
+                perimeter: () => (2*(l+b)),
+                area:() => (l*b)
+            }), 
+            2000);
+}
