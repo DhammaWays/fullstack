1c386c72687909ea856d1b4ca40acb97442bc220
Lekhraj <lekhraj@gmail.com>
2022-09-29 19:47:55 +0530

    conFusionServer: Updated to connect RESTAPI to operations on model via mongoose/MongoDB

--- backend/conFusionServer/routes/templateRouter.js
+++ backend/conFusionServer/routes/templateRouter.js
@@ -10,47 +10,78 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const templateRouter = (entity) => {
-    let entOne = entity.slice(0, -1); /* chop off last character */
-    if (entity.endsWith('es')) entOne = entOne.slice(0, -1);
-
     const gRouter = express.Router();
     gRouter.use(bodyParser.json());
 
+    const Model = require(`../models/${entity}`);
+
     gRouter.route('/')
-        .all((req, res, next) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            next();
-        })
         .get((req, res, next) => {
-            res.end(`Will send all the ${entity} to you!`);
+            Model.find({})
+                .then((data) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(data);
+                }, (err) => next(err))
+                .catch((err) => next(err));
         })
         .post((req, res, next) => {
-            res.end(`Will add the ${entity}: ` + req.body.name + ' with details: ' + req.body.description);
+            Model.create(req.body)
+                .then((data) => {
+                    console.log('Created ', data);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(data);
+                }, (err) => next(err))
+                .catch((err) => next(err));
         })
         .put((req, res, next) => {
             res.statusCode = 403;
             res.end(`PUT operation not supported on /${entity}`);
         })
         .delete((req, res, next) => {
-            res.end(`Deleting all ${entity}`);
+            Model.remove({})
+                .then((resp) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(resp);
+                }, (err) => next(err))
+                .catch((err) => next(err));
         });
 
     gRouter.route('/:Id')
         .get((req, res, next) => {
-            res.end(`Will send details of the ${entOne}: ` + req.params.Id + ' to you!');
+            Model.findById(req.params.Id)
+                .then((data) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(data);
+                }, (err) => next(err))
+                .catch((err) => next(err));
         })
         .post((req, res, next) => {
             res.statusCode = 403;
             res.end(`POST operation not supported on /${entity}/${req.params.Id}`);
         })
         .put((req, res, next) => {
-            res.write(`Updating the ${entOne}: ${req.params.Id} \n`);
-            res.end(`Will update the ${entOne}: ` + req.body.name +
-                ' with details: ' + req.body.description);
+            Model.findByIdAndUpdate(req.params.Id, {
+                $set: req.body
+            }, { new: true })
+                .then((data) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(data);
+                }, (err) => next(err))
+                .catch((err) => next(err));
         })
         .delete((req, res, next) => {
-            res.end(`Deleting ${entOne}: ` + req.params.Id);
+            Model.findByIdAndRemove(req.params.Id)
+                .then((resp) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(resp);
+                }, (err) => next(err))
+                .catch((err) => next(err));
         });
 
     return gRouter;
